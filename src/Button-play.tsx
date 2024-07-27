86ec3c14b97d1a4c6dd935d6645bad0bc9394f3f
Reynaldy Widjaja <reynaldywidjaja3@gmail.com>
2024-07-27 19:07:53 +0800

    add botan animation

---
+++ src/Button-play.tsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import style from './Button.module.css'
+
+function Button_play(){
+
+    return (
+        <>
+            <button className={style.button}></button>
+        </>
+    )
+}
+
+export default Button_play;
